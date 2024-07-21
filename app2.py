9aa9ef8703bac85e8b4581ca9dda604443756890
angelesrodriguezilan <angelesrodriguezilan@gmail.com>
2024-07-21 01:08:03 +0000

    feat: añadir mejoras en la carta de resultados

--- app2.py
+++ app2.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from PIL import Image
 import os
 
 app = Flask(__name__)
@@ -11,11 +10,12 @@ app = Flask(__name__)
 MODEL_PATH = 'mi_modelo.h5'
 model = load_model(MODEL_PATH)
 
+# Definir las etiquetas de clase
+labels = {0: 'Black Rot', 1: 'ESCA', 2: 'Healthy', 3: 'Leaf Blight'}
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', labels=labels)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -25,7 +25,7 @@ def predict():
 
         # Guardar el archivo en una carpeta temporal
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'uploads', file.filename)
+        file_path = os.path.join(basepath, 'static', 'uploads', file.filename)
         file.save(file_path)
 
         # Preprocesar la imagen para el modelo
@@ -38,23 +38,20 @@ def predict():
         result = np.argmax(prediction, axis=1)[0]  # Obtener la clase predicha
 
         # Mapear la clase predicha a una etiqueta de enfermedad
-        labels = {0: 'Black Rot', 1: 'ESCA', 2: 'Healthy', 3: 'Leaf Blight'}  # Ajustar según las etiquetas de tu modelo
-        if result in labels:
-            prediction_label = labels[result]
-        else:
-            prediction_label = "Etiqueta desconocida"
+        prediction_label = labels.get(result, "Etiqueta desconocida")
 
         # Detalles adicionales de la predicción
         prediction_confidence = np.max(prediction)  # Confianza de la predicción
         prediction_probabilities = prediction[0]  # Probabilidades de todas las clases
 
-
-
-        return render_template('index.html', prediction=prediction_label, 
-                                confidence=prediction_confidence,
-                                probabilities=prediction_probabilities.tolist())
+        # Pasar la información a la plantilla
+        return render_template('index.html', 
+                               prediction=prediction_label, 
+                               confidence=prediction_confidence,
+                               probabilities=prediction_probabilities.tolist(),
+                               uploaded_image=file.filename,
+                               labels=labels)
     return None
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 2100)))
